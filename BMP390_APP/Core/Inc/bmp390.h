a27ddff043afbc79c557a31a7cb713c2b1843270
YEK-Kayra <124110070+YEK-Kayra@users.noreply.github.com>
2023-10-29 21:19:37 +0300

    Enum declaration
    Filter enum is added

--- BMP390_APP/Core/Inc/bmp390.h
+++ BMP390_APP/Core/Inc/bmp390.h
@@ -15,8 +15,8 @@
  *
  * !Chip Id , Revision Id will be added
  * !FIFO features and Sensor Time  will be added
- * Event register will be used for
- *
+ * Event register will be used
+ * CMD register will be used
  **/
 
 #ifndef INC_BMP390_H_
@@ -24,18 +24,28 @@
 
 
 /****************************************************************/
-/*! @name       Header includes             */
+/*!@name         Header includes            				    */
 /****************************************************************/
 #include "main.h"
 #include "stdbool.h"
 #include "stdint.h"
 
+/******************************************************************************/
+/*!@name          BMP390 External Variable	     	                          */
+/******************************************************************************/
+extern double BMP390_Pressure;
+extern double BMP390_Temperature;
+extern float  BM390_Altitude;
+extern float  BMP390_gForce;
+extern float  BMP390_VerticalSpeed;
+
+
 /****************************************************************/
-/*! @name       General Macro Definitions        			    */
+/*!@name          BMP390 Registers   			    		    */
 /****************************************************************/
 
-#define BMP390_REG_ERR		 		0x02  /*Sensor Error conditions ==> bits: conf_err[2:2], cmd_err[1:1], fatal_err[0:0], */
-#define BMP390_REG_STATUS    		0x03  /*Sensor Status Flags	  ==> bits: drdy_temp[6:6], drdy_press[5:5], cmd_rdy[4:4]*/
+#define BMP390_REG_ERR		 		0x02  /*Sensor Error conditions ==> bits: conf_err[2:2], cmd_err[1:1], fatal_err[0:0] */
+#define BMP390_REG_STATUS    		0x03  /*Sensor Status Flags ==> bits: drdy_temp[6:6], drdy_press[5:5], cmd_rdy[4:4] (drdy:Data ready x)*/
 #define BMP390_REG_DATA_0    		0x04  /*24Bit pressure data of XLSB byte*/
 #define BMP390_REG_DATA_1    		0x05  /*24Bit pressure data of LSB 	byte*/
 #define BMP390_REG_DATA_2    		0x06  /*24Bit pressure data of MSB 	byte*/
@@ -48,41 +58,44 @@
  	 	 	 	 	 	 	 	 	 	 	* */
 
 #define BMP390_REG_OSR   			0x1C  /**Controls the oversampling settings for pressure and temperature measurements.
- 	 	 	 	 	 	 	 	 	 	 	* bits :  osr_t[5:3], osr_p[2:0]
+ 	 	 	 	 	 	 	 	 	 	 	* bits :  osr_t[5:3](for temperature), osr_p[2:0](for pressure)
  	 	 	 	 	 	 	 	 	 	 	* */
 
-#define BMP390_REG_OSR   			0x1C  /**Set the configuration of the output data rates
+#define BMP390_REG_ODR   			0x1D  /**Set the configuration of the output data rates
  	 	 	 	 	 	 	 	 	 	 	* bits :  odr_sel[4:0]
  	 	 	 	 	 	 	 	 	 	 	* */
 
-
-
-
-
-
-
-/******************************************************************************/
-/*!  @name         External Variable 				                          */
-/******************************************************************************/
-extern double BMP390_Pressure;
-extern double BMP390_Temperature;
-extern float  BM390_Altitude;
-extern float  BMP390_gForce;
-extern float  BMP390_VerticalSpeed;
-
-/******************************************************************************/
-/*!  @name         Enum Declarations                                  		  */
-/******************************************************************************/
-
-
+#define BMP390_REG_CONFIG		 	0x1F /**Controls the IIR filter coefficients.
+ 	 	 	 	 	 	 	 	 	 	 	* bits :  iir_filter[3:1]
+ 	 	 	 	 	 	 	 	 	 	 	* */
 
 
 /******************************************************************************/
-/*!  @name         Structure Declarations                             		  */
+/*!@name         	BMP390 Enums                                      		  */
+/******************************************************************************/
+
+/**
+ * @brief BMP390 IIR filter coefficients enumeration definition
+ */
+typedef enum{
+
+	BMP390_Filter_Coef_X0 =   0,   /* filter coefficient : 0 */
+	BMP390_Filter_Coef_X1 =   1,   /* filter coefficient : 1 */
+	BMP390_Filter_Coef_X3 =   2,   /* filter coefficient : 3 */
+	BMP390_Filter_Coef_X7 =   3,   /* filter coefficient : 7 */
+	BMP390_Filter_Coef_X15 =  4,   /* filter coefficient : 15 */
+	BMP390_Filter_Coef_X31 =  5,   /* filter coefficient : 31 */
+	BMP390_Filter_Coef_X63 =  6,   /* filter coefficient : 63 */
+	BMP390_Filter_Coef_X127 = 7    /* filter coefficient : 127 */
+
+}BMP390_FilterCoefficients;
+
+/******************************************************************************/
+/*!@name         	BMP390 Structures                                         */
 /******************************************************************************/
 
 /******************************************************************************/
-/*         			BMP280 Function prototypes            				  	  */
+/*!@name          	BMP390 Function Prototypes            			  		  */
 /******************************************************************************/
 
 //BMP390_StatusTypeDef BMP390_Init();
